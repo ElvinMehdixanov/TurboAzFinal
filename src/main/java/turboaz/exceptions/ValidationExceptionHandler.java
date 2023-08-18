f21957cecd077e2bf5deb29bda6a8fc795a7d011
elvinmehdixanov <ellvinmehdixanov@gmail.com>
2023-08-18 23:56:38 +0400

    Initial commit

--- src/main/java/turboaz/exceptions/ValidationExceptionHandler.java
+++ src/main/java/turboaz/exceptions/ValidationExceptionHandler.java
@@ -1,6 +1,7 @@
 package turboaz.exceptions;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,17 +12,21 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class ValidationExceptionHandler {
+     @ExceptionHandler
+             (MethodArgumentNotValidException.class)
+        public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex) {
+            Map<String, String> errors = new HashMap<>();
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
+            BindingResult bindingResult = ex.getBindingResult();
 
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
+            bindingResult.getAllErrors().forEach((error) -> {
+                if (error instanceof FieldError) {
+                    FieldError fieldError = (FieldError) error;
+                    errors.put(fieldError.getField(), error.getDefaultMessage());
+                }
+            });
 
-        return ResponseEntity.badRequest().body(errors);
+            return ResponseEntity.badRequest().body(errors);
+        }
     }
-}
+
